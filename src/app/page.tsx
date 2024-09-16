2a2373013ed4bc9d655fa9e71ba187987aa0b7aa
System Administrator <rodrirwigara@gmail.com>
2024-09-16 09:05:41 +0200

    compnent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='font-roboto'>
       hello cyber
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        roboto: ["var(--font-roboto)", "sans-serif"],
+        merriweather: ["var(--font-merriweather)", "serif"],
+      },
     },
   },
   plugins: [],
